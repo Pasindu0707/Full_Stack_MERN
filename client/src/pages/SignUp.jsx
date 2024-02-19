2e11194644c0f7d9e8cc683d31bfee1076026953
Pasindu0707 <123113924+Pasindu0707@users.noreply.github.com>
2024-02-20 00:38:10 +0530

    complete the sign in page functional

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,12 @@
 import { set } from 'mongoose';
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
   const [formData, setFormData] =useState({})
   const [error, setError] =useState(false)
   const [loading , setLoading] =useState(false)
+  const navigate = useNavigate()
 
   const handleChange = (e)=>{
       setFormData({...formData,[e.target.id]:e.target.value})
@@ -29,6 +30,7 @@ export default function SignUp() {
         setError(true)
         return
       }
+      navigate('/signin')
     }catch(err){
       setLoading(true)
       setError(true)
@@ -68,7 +70,7 @@ export default function SignUp() {
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an Account?</p>
-        <Link to="/sign-in" className='text-blue-500'>
+        <Link to="/signin" className='text-blue-500'>
           Sign In
         </Link>
       </div>
