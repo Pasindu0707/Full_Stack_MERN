34c46b8ed677e1a251adee63c6163dffe3ff8134
Pasindu0707 <123113924+Pasindu0707@users.noreply.github.com>
2024-02-20 00:19:03 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bycrptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js"
+import  jwt  from "jsonwebtoken"
 
 export const signup = async ( req ,res , next)=>{
 
@@ -15,4 +16,32 @@ export const signup = async ( req ,res , next)=>{
     // res.status(500).json(err.message)
     next (err)
  }
+}
+
+export const signin = async ( req ,res , next)=>{
+
+   const {email,password}= req.body
+try{
+   const validUser = await User.findOne({email})
+
+   if (!validUser){
+      return next(errorHandler(404, 'User not found'))
+   }else{
+      const validPassword = bycrptjs.compareSync(password,validUser.password)
+      
+      if(!validPassword){
+         return next(errorHandler(401, 'Wrong credentials'))
+      }else{
+         const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+         const { password : hashedPassword, ...rest} = validUser._doc // to not send the password to user
+         const expiryDate = new Date(Date.now() + 3600000) //1hour 
+   
+         res.cookie('accessToken' , token , {httpOnly:true}, expiryDate ).status(200).json(rest) //accessToken here will save in webbrowser but it saves the secret token we given in env 
+      }
+   }
+
+}catch (err){
+   // res.status(500).json(err.message)
+   next (err)
+}
 }
