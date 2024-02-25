49158c9bac3baefd30438af153f5eeea94634e66
Pasindu0707 <123113924+Pasindu0707@users.noreply.github.com>
2024-02-26 01:32:52 +0530

    add sign out user function

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,36 +1,48 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getStorage, uploadBytesResumable, ref, getDownloadURL } from 'firebase/storage';
+import { useSelector } from 'react-redux';
+import { useRef, useState, useEffect } from 'react';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart,updateUserFailure,updateUserSuccess, deleteUserStart , deleteUserFailure,deleteUserSuccess} from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+  signOut,
+} from '../redux/user/userSlice';
 
 export default function Profile() {
-  const dispatch = useDispatch()
-
-  const { currentUser, loading , error } = useSelector(state => state.user);
+  const dispatch = useDispatch();
   const fileRef = useRef(null);
   const [image, setImage] = useState(undefined);
   const [imagePercent, setImagePercent] = useState(0);
   const [imageError, setImageError] = useState(false);
   const [formData, setFormData] = useState({});
-  const [updateSuccess,setUpdateSuccess] = useState(false)
+  const [updateSuccess, setUpdateSuccess] = useState(false);
 
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   useEffect(() => {
     if (image) {
       handleFileUpload(image);
     }
   }, [image]);
-
   const handleFileUpload = async (image) => {
     const storage = getStorage(app);
     const fileName = new Date().getTime() + image.name;
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, image);
-
     uploadTask.on(
       'state_changed',
       (snapshot) => {
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         setImagePercent(Math.round(progress));
       },
       (error) => {
@@ -43,32 +55,32 @@ export default function Profile() {
       }
     );
   };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
 
-  const handleChange=(e)=>{
-    setFormData({...formData, [e.target.id]: e.target.value})
-  }
-  const handleSubmit = async(e) =>{
-    e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      dispatch(updateUserStart())
-      const res = await fetch(`/api/user/update/${currentUser._id}`,{
-        method:'POST',
-        headers:{
-          'Content-Type': 'application/json'
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formData)
-      })
-      const data = await res.json()
-      if( data.success === false){
-        dispatch(updateUserFailure(data))
-        return
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data));
+        return;
       }
-      dispatch(updateUserSuccess(data))
-      setUpdateSuccess(true)
-    }catch ( err){
-        dispatch(updateUserFailure(err))
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error));
     }
-  }
+  };
 
   const handleDeleteAccount = async () => {
     try {
@@ -87,23 +99,44 @@ export default function Profile() {
     }
   };
 
-
+  const handleSignOut = async () => {
+    try {
+      await fetch('/api/auth/signout');
+      dispatch(signOut())
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-        <input type="file" ref={fileRef} hidden accept='image/*' onChange={(e) => setImage(e.target.files[0])} />
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='file'
+          ref={fileRef}
+          hidden
+          accept='image/*'
+          onChange={(e) => setImage(e.target.files[0])}
+        />
+        {/* 
+      firebase storage rules:  
+      allow read;
+      allow write: if
+      request.resource.size < 2 * 1024 * 1024 &&
+      request.resource.contentType.matches('image/.*') */}
         <img
           src={formData.profilePicture || currentUser.profilePicture}
-          alt="Profile"
-          className='mt-2 cursor-pointer h-24 w-24 self-center rounded-full object-cover'
+          alt='profile'
+          className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2'
           onClick={() => fileRef.current.click()}
         />
         <p className='text-sm self-center'>
           {imageError ? (
-            <span className='text-red-700'>Error uploading image (file size must be less than 2 MB)</span>
+            <span className='text-red-700'>
+              Error uploading image (file size must be less than 2 MB)
+            </span>
           ) : imagePercent > 0 && imagePercent < 100 ? (
-            <span className='text-slate-700'>{`Uploading: ${imagePercent}%`}</span>
+            <span className='text-slate-700'>{`Uploading: ${imagePercent} %`}</span>
           ) : imagePercent === 100 ? (
             <span className='text-green-700'>Image uploaded successfully</span>
           ) : (
@@ -112,23 +145,23 @@ export default function Profile() {
         </p>
         <input
           defaultValue={currentUser.username}
-          type="text"
-          id="username"
+          type='text'
+          id='username'
           placeholder='Username'
           className='bg-slate-100 rounded-lg p-3'
           onChange={handleChange}
         />
         <input
           defaultValue={currentUser.email}
-          type="email"
-          id="email"
+          type='email'
+          id='email'
           placeholder='Email'
           className='bg-slate-100 rounded-lg p-3'
           onChange={handleChange}
         />
         <input
-          type="password"
-          id="password"
+          type='password'
+          id='password'
           placeholder='Password'
           className='bg-slate-100 rounded-lg p-3'
           onChange={handleChange}
@@ -138,11 +171,20 @@ export default function Profile() {
         </button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span onClick={handleDeleteAccount} className='text-red-700 cursor-pointer'>Delete Account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+        <span
+          onClick={handleDeleteAccount}
+          className='text-red-700 cursor-pointer'
+        >
+          Delete Account
+        </span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>
+          Sign out
+        </span>
       </div>
       <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
-      <p className='text-green-700 mt-5'>{updateSuccess && 'User us updated Successfully'}</p>
+      <p className='text-green-700 mt-5'>
+        {updateSuccess && 'User is updated successfully!'}
+      </p>
     </div>
   );
 }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -47,6 +47,11 @@ const userSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
           },
+          signOut:(state) =>{
+            state.currentUser=null;
+            state.loading =false;
+            state.error=false
+          }
       
     }
 })
@@ -59,6 +64,7 @@ export const { signInStart,
     updateUserSuccess,
     deleteUserFailure,
     deleteUserStart,
-    deleteUserSuccess} = userSlice.actions
+    deleteUserSuccess,
+    signOut} = userSlice.actions
 
 export default userSlice.reducer
